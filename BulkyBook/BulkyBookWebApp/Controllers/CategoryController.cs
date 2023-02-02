37457b16013a6de9571b29c2ed8ece132b81a8db
Ishan <ishanwijes@gmail.com>
2023-02-02 19:03:56 +1100

    added bootswatch theme and navbar

--- BulkyBook/BulkyBookWebApp/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBookWebApp/Controllers/CategoryController.cs
@@ -2,7 +2,11 @@
 // need this for ApplicationDbContext
 using BulkyBookWeb.Data;
 using BulkyBookWebApp.Models;
+
 using Microsoft.AspNetCore.Mvc;
+
+// need this to use FromSql()
+using Microsoft.EntityFrameworkCore;
 
 
 
@@ -11,9 +15,11 @@ namespace BulkyBookWebApp.Controllers
     public class CategoryController : Controller
     {
         // you need this for Dependency injection
+        // you need this to access the SQL database, which is _db
         private readonly ApplicationDbContext _db;
 
         // you need this for Dependency injection
+        // you need this to access the SQL database, which is _db
         public CategoryController(ApplicationDbContext db)
         {
             _db = db;
@@ -22,15 +28,12 @@ namespace BulkyBookWebApp.Controllers
 
         public IActionResult Index()
         {
-            // convert categories table in database to a list
-            // you do no have to write a SQL SELECT statement to get all the categories from the database, just do this
-            // IEnumerable<Category> is just a strongly typed datatype, we could've used var, but better to use IEnumerable<Category>
-            IEnumerable<Category> objCategoryList = _db.Categories;
 
-            Console.WriteLine(objCategoryList);
+            // you can easily query the SQL database using FromSql(), it's that easy!      
+            List<Category> objCategoryList = _db.Categories.FromSql($"SELECT * FROM Categories").ToList();
 
             // we can pass objCategoryList as an argument into View() so that it will be available in that particular View file
-            return View();
+            return View(objCategoryList);
         }
     }
 }
